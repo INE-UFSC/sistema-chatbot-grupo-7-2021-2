24005910e24aff94435d72f9e8b4bddad54d7484
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2021-11-23 12:15:31 -0300

    Finalização do sistema de bots

--- SistemaChatBot/SistemaChatBot.py
+++ SistemaChatBot/SistemaChatBot.py
@@ -1,37 +1,56 @@
 from Bots.Bot import Bot
+from Bots.BotZangado import BotZangado
+from Bots.BotFeliz import BotFeliz
+from Bots.BotTriste import BotTriste
 
 class SistemaChatBot:
     def __init__(self,nomeEmpresa,lista_bots):
         self.__empresa=nomeEmpresa
-        ##verificar se a lista de bots contém apenas bots
-        self.__lista_bots=lista_bots
+        for i in lista_bots:
+            if type(i) == Bot:
+                self.__lista_bots=lista_bots
         self.__bot = None
     
     def boas_vindas(self):
-        pass
-        ##mostra mensagem de boas vindas do sistema
-
+        print('Olá, este é o sistema de de chatbots da empresa CrazyBots')
+        
     def mostra_menu(self):
-        pass
-        ##mostra o menu de escolha de bots
+        print('''Os chat bots disponíveis no momento são:
+              0 - Bot: Yoda - Mensagem de apresentação: Grrrrr. Meu nome é Yoda e eu te odeio!
+              1 - Bot: Feliciano - Mensagem de apresentação: Hahaha. Meu nome é Feliciano e eu estou muito feliz!
+              2 - Bot: Universitário - Mensagem de apresentação: snif snif. Meu nome é universitário e eu estou triste...''')
     
     def escolhe_bot(self):
-        pass
-        ##faz a entrada de dados do usuário e atribui o objeto ao atributo __bot 
+        num_bot = int(input('Digite o número do chat bot desejado:'))
+        
+        if num_bot == 0:
+            self.__bot = BotZangado('Yoda')
+        elif num_bot == 1:
+            self.__bot = BotFeliz('Feliciano')
+        elif num_bot == 2: 
+            self.__bot = BotTriste('Universitário')
+        else:
+            print('Bot inválido')
 
     def mostra_comandos_bot(self):
-        pass
-        ##mostra os comandos disponíveis no bot escolhido
-
+        print(self.__bot.mostra_comandos())
+        
     def le_envia_comando(self):
-        pass
-        ##faz a entrada de dados do usuário e executa o comando no bot ativo
+        global sair
+        comando = int(input('Digite o comando desejado (ou -1 para fechar o comando):'))
+        sair = False
+        if comando == -1:
+            sair = True
+            pass
+        else: 
+            print(self.__bot.executa_comando(comando))
 
     def inicio(self):
-        pass
-        ##mostra mensagem de boas-vindas do sistema
-        ##mostra o menu ao usuário
-        ##escolha do bot      
-        ##mostra mensagens de boas-vindas do bot escolhido
-        ##entra no loop de mostrar comandos do bot e escolher comando do bot até o usuário definir a saída
-        ##ao sair mostrar a mensagem de despedida do bot
+        self.boas_vindas()
+        self.mostra_menu()
+        self.escolhe_bot()
+        while True:
+            self.mostra_comandos_bot()
+            self.le_envia_comando()
+            if sair == True:
+                break
--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #encoding: utf-8
+from Bots.BotTriste import BotTriste
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
+from Bots.BotFeliz import BotFeliz
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda")]
+lista_bots = [BotZangado("Yoda"), BotTriste("Universitário")]
 
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
