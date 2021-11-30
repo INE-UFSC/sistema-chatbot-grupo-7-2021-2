b2ff92361c433d180210a623eb8c309f2bfccd74
Rodrigo Santos de Carvalho <rodrigosc2401@gmail.com>
2021-11-30 11:54:50 -0300

    .

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from Bots.BotZangado import BotZangado
 from Bots.BotFeliz import BotFeliz
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda"), BotTriste("Universitário"), BotFeliz("Feliciano"), BotGamer("JOID")]
+lista_bots = [BotZangado("Yoda"), BotTriste("Universitário"), BotFeliz("Feliciano")]
 sys = scb.SistemaChatBot("CrazyBots",lista_bots)
 sys.inicio()
